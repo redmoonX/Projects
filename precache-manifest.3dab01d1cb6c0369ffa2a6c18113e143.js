f164c1a3360f04200e006ace7e7953755a43195b
redmoonX <52355699+redmoonX@users.noreply.github.com>
2019-09-07 19:13:30 -0500

    swapi react build folder

---
+++ precache-manifest.3dab01d1cb6c0369ffa2a6c18113e143.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0755ab0f51eacd7e83913e721dbad763",
+    "url": "/Projects/index.html"
+  },
+  {
+    "revision": "e3dfd9d23f5865a57173",
+    "url": "/Projects/static/css/main.318e3027.chunk.css"
+  },
+  {
+    "revision": "d6596e493dbf4a19e6a3",
+    "url": "/Projects/static/js/2.7b2a79c6.chunk.js"
+  },
+  {
+    "revision": "e3dfd9d23f5865a57173",
+    "url": "/Projects/static/js/main.3a141c0a.chunk.js"
+  },
+  {
+    "revision": "f12c87fb5951217cf5de",
+    "url": "/Projects/static/js/runtime~main.5543331f.js"
+  }
+]);
